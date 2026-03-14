3ca48f93dee54535cee716152f18a02274b65deb
LekoArts <lekoarts@gmail.com>
2026-03-14 16:25:05 +0100

    initial

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+	formatters: true,
+	svelte: true,
+	stylistic: {
+		indent: 'tab',
+		quotes: 'single',
+		semi: false,
+	},
+})
